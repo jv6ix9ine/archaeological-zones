ada465f62e96decdbefcb5d06dc25195b0437133
Joaht Vera <joaht.vera@cancunit.com>
2023-11-22 09:30:03 -0500

    build errors

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import HomePage from './HomePage'
 import StateService from '@/src/services/states'
 import ZoneService from '@/src/services/zones'
+import StateController from '@/src/server/controller/state'
 
 const Home = async() => {
-  const states = (await StateService.getAll()).data
-  
+  const statesRequest = await StateController.getAll()
+  const states = await statesRequest.json()
   return (
     <HomePage states={states ?? []}/>
   )
---
+++ app/zones/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ZonesPage = () => {
+  return (
+    <div>ZonesPage</div>
+  )
+}
+
+export default ZonesPage
